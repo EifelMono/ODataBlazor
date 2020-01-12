fa42a470a97f6458277cd72d14552db19f11e706
Andreas Klapperich <andreas@klapperich.de>
2020-01-12 15:52:20 +0100

    CHG:

---
+++ src/ODataBlazor.Server/Components/ComponentCore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace ODataBlazor.Server.Components
+{
+    public class ComponentCore : ComponentBase
+    {
+        public ComponentCore() : base()
+        {
+        }
+
+        public new void StateHasChanged()
+            => base.StateHasChanged();
+    }
+}
---
+++ src/ODataBlazor.Server/Extensions/ComponentCoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ODataBlazor.Server.Components;
+
+namespace ODataBlazor.Server.Extensions
+{
+    public static class ComponentCoreExtensions
+    {
+        public static async Task<bool> ShowModalAsync(this ComponentCore thisValue, string title, string text)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            thisValue.StateHasChanged();
+            return false;
+        }
+    }
+}
